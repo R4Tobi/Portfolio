56fa7a60477838078a18056f6455e4d1034fddc7
R4Tobi <62656419+R4Tobi@users.noreply.github.com>
2023-05-10 22:15:23 +0200

    init nav

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,33 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <NavHeader />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NavHeader from './components/NavHeader.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    NavHeader
   }
 }
 </script>
 
 <style>
+*{
+  box-sizing: border-box;
+}
+
+html, body{
+  margin: 0;
+  padding: 0;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/NavHeader.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="nav">
+    <div class="left">To Be. Web Solutions.</div>
+    <div class="right">
+      <ul>
+        <li>Über mich</li>        
+        <li>Projekte</li>        
+        <li>Socials</li>        
+        <li>Skills</li>        
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NavHeader',
+  props: {
+    urls: Array
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+@import "../stylesheets/nav.scss";
+</style>
