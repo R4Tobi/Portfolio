d3ce5451b63f621c2471c9a1afc2449ff69ee306
R4Tobi <R4Tobi@users.noreply.github.com>
2023-06-07 22:46:07 +0200

    updated privacy

--- src/views/Privacy.vue
+++ src/views/Privacy.vue
@@ -1,18 +1,166 @@
 <template>
-<h2>Datenschutz</h2>
-<p>
-  Diese website verwendet Cookies um zu funktionieren, dabei werden keine statistischen Cookies oder Daten erhoben, die das Benutzerverhalten anlysieren und spezifizieren. Es werden keine Daten an dritte weitergegeben werden. Cookies die durch externe Websiten (wir zum Besispiel Werbung              )
-</p>
+  <div class="datenschutz">
+    <h1>Datenschutzerklärung</h1>
+    <h2>1. Datenschutz auf einen Blick</h2>
+    <h3>Allgemeine Hinweise</h3>
+    <p>
+      Die folgenden Hinweise geben einen einfachen Überblick darüber, was mit Ihren personenbezogenen Daten passiert, wenn Sie diese Website besuchen. Personenbezogene Daten sind alle Daten, mit denen Sie persönlich identifiziert werden können. Ausführliche Informationen zum Thema Datenschutz entnehmen Sie unserer unter diesem Text aufgeführten Datenschutzerklärung.
+    </p>
+    <h3>Datenerfassung auf dieser Website</h3>
+    <h4>Verantwortliche Stelle</h4>
+    <p>
+      Die Datenverarbeitung auf dieser Website erfolgt durch den Websitebetreiber.
+Dessen Kontaktdaten können Sie dem Abschnitt "Hinweis zur Verantwortlichen
+Stelle" oder dem "Impressum" in dieser Datenschutzerklärung entnehmen.
+    </p>
+    <h4>Wie erfassen wir Ihre Daten?</h4>
+    <p>
+      Ihre Daten werden zum einen dadurch erhoben, dass Sie uns diese mitteilen.
+      Hierbei kann es sich z. B. um Daten handeln, die Sie in ein Kontaktformular
+      eingeben. Andere Daten werden automatisch oder nach Ihrer Einwilligung beim
+      Besuch der Website durch unsere IT-Systeme erfasst. Das sind vor allem
+      technische Daten (z. B. Internetbrowser, Betriebssystem oder Uhrzeit des
+      Seitenaufrufs). Die Erfassung dieser Daten erfolgt automatisch, sobald Sie diese
+      Website betreten.
+    </p>
+    <h4>Wofür nutzen wir Ihre Daten?</h4>
+    <p>
+      Ein Teil der Daten wird erhoben, um eine fehlerfreie Bereitstellung der Website
+      zu gewährleisten. Andere Daten können zur Analyse Ihres Nutzerverhaltens
+      verwendet werden.
+    </p>
+    <h3>Welche Rechte haben Sie bezüglich ihrer Daten</h3>
+    <p>
+      Sie haben jederzeit das Recht, unentgeltlich Auskunft über Herkunft, Empfänger
+      und Zweck Ihrer gespeicherten personenbezogenen Daten zu erhalten.
+      Sie haben außerdem ein Recht, die Berichtigung oder Löschung dieser Daten zu verlangen.<br />
+      Wenn Sie eine Einwilligung zur Datenverarbeitung erteilt haben, können Sie diese
+      Einwilligung jederzeit für die Zukunft widerrufen.<br />
+      Außerdem haben Sie das Recht, unter bestimmten Umständen die Einschränkung der Verarbeitung Ihrer
+      personenbezogenen Daten zu verlangen.
+      Des Weiteren steht Ihnen ein Beschwerderecht bei der zuständigen Aufsichtsbehörde zu.<br/>
+      Hierzu sowie zu weiteren Fragen zum Thema Datenschutz können Sie sich jederzeit an uns wenden.
+    </p>
+    <h2>2. Hosting & Daten zur Analyse</h2>
+    <h3>Externes Hosting</h3>
+    <p>
+      Diese Website wird extern gehostet.<br /> 
+      Die personenbezogenen Daten, die auf dieser Website erfasst werden, werden auf den Servern des Hosters / der Hoster gespeichert.<br /> 
+      Hierbei kann es sich v. a. um IP-Adressen, Kontaktanfragen, Meta- und Kommunikationsdaten, Vertragsdaten, Kontaktdaten, Namen, Websitezugriffe und
+      sonstige Daten, die über eine Website generiert werden, handeln.<br /> 
+      Das externe Hosting erfolgt zum Zwecke der Vertragserfüllung gegenüber unseren potenziellen und bestehenden Kunden (Art. 6 Abs. 1 lit. b DSGVO) und im Interesse einer
+      sicheren, schnellen und effizienten Bereitstellung unseres Online-Angebots durch
+      einen professionellen Anbieter (Art. 6 Abs. 1 lit. f DSGVO).<br />
+      Sofern eine entsprechende Einwilligung abgefragt wurde, erfolgt die Verarbeitung
+      ausschließlich auf Grundlage von Art. 6 Abs. 1 lit. a DSGVO und § 25 Abs. 1
+      TTDSG, soweit die Einwilligung die Speicherung von Cookies oder den Zugriff auf
+      Informationen im Endgerät des Nutzers (z. B. Device-Fingerprinting) im Sinne des
+      TTDSG umfasst.<br />
+      Die Einwilligung ist jederzeit widerrufbar.<br />
+      Unser(e) Hoster wird bzw. werden Ihre Daten nur insoweit verarbeiten, wie dies zur Erfüllung seiner
+      Leistungspflichten erforderlich ist und unsere Weisungen in Bezug auf diese
+      Daten befolgen. Wir setzen folgende(n) Hoster ein:<br />
+      STRATO Hosting
+    </p>
+    <h3>Auftragsverarbeitung</h3>
+    <p>
+      Wir haben einen Vertrag über Auftragsverarbeitung (AVV) mit dem oben genannten
+      Anbieter geschlossen.<br />
+      Hierbei handelt es sich um einen datenschutzrechtlich
+      vorgeschriebenen Vertrag, der gewährleistet, dass dieser die personenbezogenen
+      Daten unserer Websitebesucher nur nach unseren Weisungen und unter Einhaltung
+      der DSGVO verarbeitet.
+    </p>
+    <h2>3. Allgemeine Hinweise und Pflichtinformationen</h2>
+    <h3>Datenschutz</h3>
+    <p>
+      Die Betreiber dieser Seiten nehmen den Schutz Ihrer persönlichen Daten sehr ernst.<br /> 
+      Wir behandeln Ihre personenbezogenen Daten vertraulich und entsprechend
+      den gesetzlichen Datenschutzvorschriften sowie dieser Datenschutzerklärung.
+      Wenn Sie diese Website benutzen, werden verschiedene personenbezogene Daten erhoben.<br/>
+      Personenbezogene Daten sind Daten, mit denen Sie persönlich identifiziert werden können.<br /> 
+      Die vorliegende Datenschutzerklärung erläutert, welche Daten wir erheben
+      und wofür wir sie nutzen.
+      Sie erläutert auch, wie und zu welchem Zweck das geschieht.<br/> 
+      Wir weisen darauf hin, dass die Datenübertragung im Internet (z. B.
+      bei der Kommunikation per E-Mail) Sicherheitslücken aufweisen kann.
+      Ein lückenloser Schutz der Daten vor dem Zugriff durch Dritte ist nicht möglich.
+    </p>
+    <h3>Hinweise zur verantwortlichen Stelle</h3>
+    <p>Siehe Impressum</p>
+    <h3>Speicherdauer</h3>
+    <p>
+      Soweit innerhalb dieser Datenschutzerklärung keine speziellere Speicherdauer
+      genannt wurde, verbleiben Ihre personenbezogenen Daten bei uns, bis der Zweck
+      für die Datenverarbeitung entfällt. Wenn Sie ein berechtigtes Löschersuchen
+      geltend machen oder eine Einwilligung zur Datenverarbeitung widerrufen, werden
+      Ihre Daten gelöscht, sofern wir keine anderen rechtlich zulässigen Gründe für
+      die Speicherung Ihrer personenbezogenen Daten haben (z. B. steuer- oder
+      handelsrechtliche Aufbewahrungsfristen); im letztgenannten Fall erfolgt die
+      Löschung nach Fortfall dieser Gründe.
+    </p>
+    <h3>^3. Allgemeine Hinweise zu den Rechtsgrundlagen der Datenverarbeitung auf dieser Website</h3>
+    <p>
+      Sofern Sie in die Datenverarbeitung eingewilligt haben, verarbeiten wir Ihre
+      personenbezogenen Daten auf Grundlage von Art. 6 Abs. 1 lit. a DSGVO bzw. Art. 9
+      Abs. 2 lit. a DSGVO, sofern besondere Datenkategorien nach Art. 9 Abs. 1 DSGVO
+      verarbeitet werden. Im Falle einer ausdrücklichen Einwilligung in die
+      Übertragung personenbezogener Daten in Drittstaaten erfolgt die
+      Datenverarbeitung außerdem auf Grundlage von Art. 49 Abs. 1 lit. a DSGVO. Sofern
+Sie in die Speicherung von Cookies oder in den Zugriff auf Informationen in Ihr
+Endgerät (z. B. via Device-Fingerprinting) eingewilligt haben, erfolgt die
+Datenverarbeitung zusätzlich auf Grundlage von § 25 Abs. 1 TTDSG. Die
+Einwilligung ist jederzeit widerrufbar. Sind Ihre Daten zur Vertragserfüllung
+oder zur Durchführung vorvertraglicher Maßnahmen erforderlich, verarbeiten wir
+Ihre Daten auf Grundlage des Art. 6 Abs. 1 lit. b DSGVO. Des Weiteren
+verarbeiten wir Ihre Daten, sofern diese zur Erfüllung einer rechtlichen
+Verpflichtung erforderlich sind auf Grundlage von Art. 6 Abs. 1 lit. c DSGVO.
+Die Datenverarbeitung kann ferner auf Grundlage unseres berechtigten Interesses
+nach Art. 6 Abs. 1 lit. f DSGVO erfolgen. Über die jeweils im Einzelfall
+einschlägigen Rechtsgrundlagen wird in den folgenden Absätzen dieser
+Datenschutzerklärung informiert.
+    </p>
+    <h3>Widerruf Ihrer Einwilligung zur Datenverarbeitung</h3>
+    <p>
+Viele Datenverarbeitungsvorgänge sind nur mit Ihrer ausdrücklichen Einwilligung
+möglich. Sie können eine bereits erteilte Einwilligung jederzeit widerrufen. Die
+Rechtmäßigkeit der bis zum Widerruf erfolgten Datenverarbeitung bleibt vom
+Widerruf unberührt.
+    </p>
+  </div>
 </template>
 
 <script>
 export default {
   // eslint-disable-next-line vue/multi-word-component-names
-  name: 'Privacy',
-}
+  name: "Privacy",
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
 @import "../stylesheets/main.scss";
+.datenschutz{
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+
+  h1 { 
+    margin-top: 100px;
+    margin-bottom: -20px;
+    font-size: 48px;
+  }
+
+  h2{
+    margin-top: 100px; 
+    font-size: 32px;
+  }
+
+  p{
+    width: 90vw;
+    font-size: 18px;
+  }
+}
 </style>
