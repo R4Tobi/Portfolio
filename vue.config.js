c4318513332cf705f9318e8e990f9d0efb41b00e
R4Tobi <R4Tobi@users.noreply.github.com>
2023-06-14 21:03:26 +0200

    added prerendering for SEO

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,18 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+
+  pluginOptions: {
+    prerenderSpa: {
+      registry: undefined,
+      renderRoutes: [
+        '/',
+        '/home',
+        '/projects'
+      ],
+      useRenderEvent: true,
+      headless: true,
+      onlyProduction: true
+    }
+  }
 })
