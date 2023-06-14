996020e3af4b117b8e564cad4a9b513430f00ea8
R4Tobi <R4Tobi@users.noreply.github.com>
2023-06-14 22:15:46 +0200

    Revert "added prerendering for SEO"
    This reverts commit c4318513332cf705f9318e8e990f9d0efb41b00e.

--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true,
-
-  pluginOptions: {
-    prerenderSpa: {
-      registry: undefined,
-      renderRoutes: [
-        '/',
-        '/home',
-        '/projects'
-      ],
-      useRenderEvent: true,
-      headless: true,
-      onlyProduction: true
-    }
-  }
+  transpileDependencies: true
 })
