6f94428bc90fd86cc780f0285b0d777a2e6f45f0
R4Tobi <R4Tobi@users.noreply.github.com>
2023-06-14 22:24:27 +0200

    finished privacy

--- src/views/Privacy.vue
+++ src/views/Privacy.vue
@@ -174,7 +174,7 @@ Wenn Sie widersprechen, werden Ihre personenbezogenen Daten anschließend nicht 
     </p>
     <h3>SSL- bzw. TLS-Verschlüsselung</h3>
     <p>
-      Diese Seite nutzt aus Sicherheitsgründen und zum Schutz der Übertragung vertraulicher Inhalte, wie zum Beispiel Bestellungen oder Anfragen, die Sie an uns als Seitenbetreiber senden, eine SSL- bzw. TLS-Verschlüsselung. Eine verschlüsselte Verbindung erkennen Sie daran, dass die Adresszeile des Browsers von „http://“ auf „https://“ wechselt und an dem Schloss-Symbol in Ihrer Browserzeile.
+      Diese Seite nutzt aus Sicherheitsgründen und zum Schutz der Übertragung vertraulicher Inhalte, wie zum Beispiel Bestellungen oder Anfragen, die Sie an uns als Seitenbetreiber senden, eine SSL- bzw. TLS-Verschlüsselung. Eine verschlüsselte Verbindung erkennen Sie daran, dass die Adresszeile des Browsers von „http://“ auf „https://“ wechselt und an dem Schloss-Symbol in Ihrer Browserzeile. Sollte dies nicht automatisch der Fall sein, wird darum gebeten, dass sie das Protokoll selber von "http" auf "https" ändern.
       <br />
       Wenn die SSL- bzw. TLS-Verschlüsselung aktiviert ist, können die Daten, die Sie an uns übermitteln, nicht von Dritten mitgelesen werden.
     </p>
